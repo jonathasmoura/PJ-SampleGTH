2f7788fbd4a6c7f5641087bb6fa8d4a2216ff29f
Jonathas Moura <jonathas.development@gmail.com>
2024-02-15 11:19:10 -0300

    Criação de clase de contexto para criação do banco de dados.

---
+++ GTH.API/DataContexts/PJGTHContext.cs
@@ -0,0 +1,18 @@
+﻿using GTH.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GTH.API.DataContexts
+{
+	public class PJGTHContext : DbContext
+	{
+		public PJGTHContext(DbContextOptions<PJGTHContext> opt) : base(opt) { }
+
+		public DbSet<Usuario> Usuarios { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
